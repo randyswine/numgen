b6de148ed1c18f423773f6b9762af03b39fc2594
randyswine <randyswine@gmail.com>
2021-09-17 17:14:39 +0400

    Generator feedback implemented

--- numbers/Generator.go
+++ numbers/Generator.go
@@ -10,17 +10,18 @@ import (
 // generator - объект, выполняющий генерацию случайного числа в диапазоне от 0 до lim.
 // Сгенерированное число передаётся в канал out.
 type generator struct {
-	timeout    time.Duration      // Таймаут частоты генерации числа.
-	lim        int64              // Максимально возможное значение числа.
-	cmds       <-chan control.Cmd // Команды, управляющие рабочим циклом генератора. Команды изменяют состояние генератора.
-	out        chan<- int         // Сгенерированное число передаётся в out.
-	randomizer *rand.Rand         // Генератор случайных чисел.
-	state      control.State      // Состояние генератора. Влияет на рабочий цикл.
-	ticker     *time.Ticker       // Тикер генерации числа.
+	timeout    time.Duration         // Таймаут частоты генерации числа.
+	lim        int64                 // Максимально возможное значение числа.
+	cmds       <-chan control.Cmd    // Команды, управляющие рабочим циклом генератора. Команды изменяют состояние генератора.
+	feedback   chan<- control.Signal // Обратная связь генератора (см. dispatcher).
+	out        chan<- int            // Сгенерированное число передаётся в out.
+	randomizer *rand.Rand            // Генератор случайных чисел.
+	state      control.State         // Состояние генератора. Влияет на рабочий цикл.
+	ticker     *time.Ticker          // Тикер генерации числа.
 }
 
 // New возвращает ссылку на новый экземпляр генератора случайных чисел.
-func New(timeout time.Duration, lim int64, cmds <-chan control.Cmd, out chan<- int) *generator {
+func New(timeout time.Duration, lim int64, cmds <-chan control.Cmd, feedback chan<- control.Signal, out chan<- int) *generator {
 	if timeout == 0 {
 		panic(fmt.Errorf("don't use value 0 of timeout"))
 	}
@@ -28,6 +29,7 @@ func New(timeout time.Duration, lim int64, cmds <-chan control.Cmd, out chan<- i
 		timeout:    timeout,
 		lim:        lim,
 		cmds:       cmds,
+		feedback:   feedback,
 		out:        out,
 		state:      control.Waiting,
 		randomizer: rand.New(rand.NewSource(lim)),
@@ -63,13 +65,15 @@ func (g *generator) handleCommand(cmd control.Cmd) {
 	case control.Start:
 		g.state = control.Worked
 		g.ticker.Reset(g.timeout * time.Second)
+		g.feedback <- control.Success
 	case control.Stop:
 		g.ticker.Stop()
 		g.state = control.Waiting
-
+		g.feedback <- control.Success
 	case control.Destroy:
 		g.ticker.Stop()
 		g.state = control.Waiting
+		g.feedback <- control.Success
 	}
 }
 
